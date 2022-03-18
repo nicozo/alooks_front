72085b97a1f81546eec558eee2928ac8fb441e19
nicozo <niczo.soccer161@gmail.com>
2022-03-18 23:58:47 +0900

    Update: フッターを作成

--- components/App/Footer.vue
+++ components/App/Footer.vue
@@ -1,11 +1,24 @@
 <template>
   <v-footer>
-    AppFooter.vue
+    <v-col
+      cols="12"
+      class="py-0"
+    >
+      <div class="text-center">
+        &copy;2022
+        <strong>{{ appName }}</strong>
+      </div>
+    </v-col>
   </v-footer>
 </template>
 
 <script>
 export default {
-  name: 'AppFooter'
+  name: 'AppFooter',
+  data ({ $config: { appName } }) {
+    return {
+      appName
+    }
+  }
 }
 </script>
