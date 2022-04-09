16a2ce8fc17a81076515abc595b14f84017635b3
nicozo <niczo.soccer161@gmail.com>
2022-04-09 17:36:10 +0900

    Update: nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,6 +64,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    // クロスオリジンでCookieを共有する
+    credentials: true
   },
 
   // いらないかも
