424fd0d245631074f7527e8ba9a3041d8dda5a29
nicozo <niczo.soccer161@gmail.com>
2022-05-22 14:25:32 +0900

    Update: profile.vue

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
-    profile.vue
+    {{ user }}
   </div>
 </template>
 
 <script>
 export default {
   name: 'ProfilePage',
-  layout: 'loggedIn'
+  layout: 'loggedIn',
+  data () {
+    return {
+      user: {
+        name: '',
+        self_introduction: '',
+        date_of_birth: null,
+        sex: null,
+        game_id: ''
+      }
+    }
+  },
+  computed: {
+    authUser () {
+      return this.$auth.user
+    }
+  },
+  created () {
+    this.user = Object.assign({}, this.authUser)
+  }
 }
 </script>
