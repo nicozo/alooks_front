5ff05dc4ab7b46d73ad34bac3a35a050189c5d39
nicozo <niczo.soccer161@gmail.com>
2022-05-26 21:11:49 +0900

    Fix: フォームのバリデーションが動作しないバグを修正

--- components/User/UserFormDateOfBirth.vue
+++ components/User/UserFormDateOfBirth.vue
@@ -1,42 +1,42 @@
 <template>
   <v-row>
     <v-col>
-      <validation-provider
-        v-slot="{ errors }"
-        name="生年月日"
-        rules="required"
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
       >
-        <v-menu
-          ref="menu"
-          v-model="menu"
-          :close-on-content-click="false"
-          transition="scale-transition"
-          offset-y
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field
-              :value="dateOfBirth"
-              label="生年月日"
-              :error-messages="errors"
-              outlined
-              readonly
-              v-bind="attrs"
-              v-on="on"
-            />
-          </template>
-          <v-date-picker
+        <template v-slot:activator="{ on, attrs }">
+          <validation-provider
+            v-slot="{ errors }"
+            name="生年月日"
+            rules="required"
+          >
+          <v-text-field
             :value="dateOfBirth"
-            no-title
-            locale="jp-ja"
-            :day-format="date => new Date(date).getDate()"
-            :active-picker.sync="activePicker"
-            :max="(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)"
-            min="1950-01-01"
-            @change="save, $emit('update:dateOfBirth', $event)"
+            label="生年月日"
+            :error-messages="errors"
+            outlined
+            readonly
+            v-bind="attrs"
+            v-on="on"
           />
-        </v-menu>
-      </validation-provider>
+          </validation-provider>
+        </template>
+        <v-date-picker
+          :value="dateOfBirth"
+          no-title
+          locale="jp-ja"
+          :day-format="date => new Date(date).getDate()"
+          :active-picker.sync="activePicker"
+          :max="(new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10)"
+          min="1950-01-01"
+          @change="save, $emit('update:dateOfBirth', $event)"
+        />
+      </v-menu>
     </v-col>
   </v-row>
 </template>
