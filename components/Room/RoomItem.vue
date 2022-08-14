5c09b27217f569188c307ad4d529979ba528b0b2
nicozo <niczo.soccer161@gmail.com>
2022-08-14 18:16:28 +0900

    Update: リクエストボタンをクリックすると詳細に遷移するバグを修正

--- components/Room/RoomItem.vue
+++ components/Room/RoomItem.vue
@@ -4,8 +4,6 @@
     rounded-xl
     hover
     raised
-    nuxt
-    :to="`rooms/${room.id}`"
   >
     <div v-show="isRoomClosing(room.application_deadline)">
       <v-overlay
@@ -19,57 +17,63 @@
       </v-overlay>
     </div>
 
-    <v-container>
-      <div class="text-right">
-        <v-chip
-          color="red"
-          dark
-        >
-          あと{{ room.recruitment_number }}人募集
-        </v-chip>
-      </div>
+    <v-card
+      flat
+      nuxt
+      :to="`rooms/${room.id}`"
+    >
+      <v-container>
+          <div class="text-right">
+            <v-chip
+              color="red"
+              dark
+            >
+              あと{{ room.recruitment_number }}人募集
+            </v-chip>
+          </div>
 
-      <v-card-title>
-        {{ room.title }}
-      </v-card-title>
+          <v-card-title>
+            {{ room.title }}
+          </v-card-title>
 
-      <v-divider />
+          <v-divider />
 
-      <v-layout justify-center>
-        <template v-if="room.host.avatar_url">
-          <v-avatar size="100" class="my-4">
-            <img :src="room.host.avatar_url" alt="プロフィール画像です">
-          </v-avatar>
-        </template>
-        <template v-else>
-          <v-avatar size="100" class="my-4">
-            <img :src="defaultAvatarSrc" alt="プロフィール画像です">
-          </v-avatar>
-        </template>
-      </v-layout>
+          <v-layout justify-center>
+            <template v-if="room.host.avatar_url">
+              <v-avatar size="100" class="my-4">
+                <img :src="room.host.avatar_url" alt="プロフィール画像です">
+              </v-avatar>
+            </template>
+            <template v-else>
+              <v-avatar size="100" class="my-4">
+                <img :src="defaultAvatarSrc" alt="プロフィール画像です">
+              </v-avatar>
+            </template>
+          </v-layout>
 
-      <v-card-text>
-        {{ $t('room.platform') }}：{{ room.platform }}
-      </v-card-text>
-      <v-card-text>
-        {{ $t('room.game_mode') }}：{{ room.game_mode }}
-      </v-card-text>
-      <v-card-text>
-        {{ $t('room.rank_tier') }}：{{ room.rank_tier }}
-      </v-card-text>
-      <v-card-text>
-        {{ $t('room.application_deadline') }}：{{ timeToDeadline }}
-      </v-card-text>
+          <v-card-text>
+            {{ $t('room.platform') }}：{{ room.platform }}
+          </v-card-text>
+          <v-card-text>
+            {{ $t('room.game_mode') }}：{{ room.game_mode }}
+          </v-card-text>
+          <v-card-text>
+            {{ $t('room.rank_tier') }}：{{ room.rank_tier }}
+          </v-card-text>
+          <v-card-text>
+            {{ $t('room.application_deadline') }}：{{ timeToDeadline }}
+          </v-card-text>
+      </v-container>
+    </v-card>
 
-      <v-card-actions>
-        <app-join-request-button
-          :room="room"
-          :authUser="authUser"
-          :invalid="invalid"
-          :applications="applications"
-        />
-      </v-card-actions>
-    </v-container>
+    <v-card-actions>
+      <app-join-request-button
+        :room="room"
+        :authUser="authUser"
+        :invalid="invalid"
+        :applications="applications"
+      />
+    </v-card-actions>
   </v-card>
 </template>
 
