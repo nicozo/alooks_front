acbe8ed688c14eeb7dad3bde44f81e25168efc96
nicozo <niczo.soccer161@gmail.com>
2022-09-20 22:11:48 +0900

    Add: プロフィールへ案内するモーダルをコンポーネント化

---
+++ components/App/AppGuideProfile.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-dialog
+    v-model="profileDialog"
+    persistent
+    max-width="600px"
+  >
+    <v-card>
+      <v-container>
+        <v-card-title class="justify-center">
+          <span class="text-h5">
+            まずはプロフィールを完成させよう！
+          </span>
+        </v-card-title>
+
+        <v-card-actions class="justify-center">
+          <v-btn
+            color="success"
+            nuxt
+            to="/profile"
+          >
+            プロフィールへ
+          </v-btn>
+        </v-card-actions>
+      </v-container>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: 'AppGuideProfile',
+  props: {
+    profileDialog: {
+      type: Boolean,
+      required: true
+    }
+  }
+}
+</script>
