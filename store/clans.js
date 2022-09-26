2395ec71f55017c1313541c7514db7159a1de44a
nicozo <niczo.soccer161@gmail.com>
2022-09-26 22:25:00 +0900

    Update: 1つのクラン情報を保持するVuexを定義

--- store/clans.js
+++ store/clans.js
@@ -1,19 +1,27 @@
 export const state = () => ({
-  clans: []
+  clans: [],
+  clan: {}
 })
 
 export const getters = {
-  clans: state => state.clans
+  clans: state => state.clans,
+  clan: state => state.clan
 }
 
 export const mutations = {
   setClans (state, clans) {
     state.clans = clans
+  },
+  setClan (state, clan) {
+    state.clan = clan
   }
 }
 
 export const actions = {
   getClans ({ commit }, res) {
     commit('setClans', res)
+  },
+  getClan ({ commit }, res) {
+    commit('setClan', res)
   }
 }
