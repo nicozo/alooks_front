13f8d8db2694d66de1b965ed185d3414995d53a5
nicozo <niczo.soccer161@gmail.com>
2022-09-27 00:30:35 +0900

    Add: クラン募集詳細画面を作成

---
+++ components/Clan/ClanInformation.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-card
+    id="clan-information"
+    flat
+  >
+    <v-card-title>
+      クラン情報
+    </v-card-title>
+
+    <v-divider />
+
+    <v-card-text>
+      在籍人数：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ information.enrollment }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      年齢層：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ information.enrollment_age }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      活動時間：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ information.activity_time }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      スナイプや大会等
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ information.snipe }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      クラン内の連絡手段：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ information.contact_means }}
+      </v-chip>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ClanInformation',
+  props: {
+    information: {
+      type: Object,
+      default: () => {},
+      require: true,
+      enrollment: {
+        type: String,
+        require: true
+      },
+      enrollment_age: {
+        type: String,
+        require: true
+      },
+      activity_time: {
+        type: String,
+        require: true
+      },
+      snipe: {
+        type: String,
+        require: true
+      },
+      contact_means: {
+        type: Boolean,
+        require: true
+      }
+    }
+  }
+}
+</script>
---
+++ components/Clan/ClanRequirements.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-card
+    id="clan-requirements"
+    flat
+  >
+    <v-card-title>
+      募集要項
+    </v-card-title>
+
+    <v-divider />
+
+    <v-card-text>
+      プラットフォーム：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ requirements.platform }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      年齢：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ requirements.age }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      ログイン回数：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ requirements.required_login }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      最高ランク：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ requirements.required_ranked }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      VCの有無：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ requirements.required_vc }}
+      </v-chip>
+    </v-card-text>
+
+    <v-card-text>
+      求める人物像：
+      <v-chip
+        color="indigo darken-4"
+        dark
+        outlined
+      >
+        {{ requirements.personality }}
+      </v-chip>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ClanRequirements',
+  props: {
+    requirements: {
+      type: Object,
+      default: () => {},
+      require: true,
+      platform: {
+        type: String,
+        require: true
+      },
+      age: {
+        type: String,
+        require: true
+      },
+      required_login: {
+        type: String,
+        require: true
+      },
+      required_ranked: {
+        type: String,
+        require: true
+      },
+      required_vc: {
+        type: Boolean,
+        require: true
+      },
+      personality: {
+        type: String,
+        require: true
+      }
+    }
+  }
+}
+</script>
