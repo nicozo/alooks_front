2fb5ef8bd4d5d2077e1c23ac6502621627191cc6
nicozo <niczo.soccer161@gmail.com>
2022-09-26 13:07:04 +0900

    Update: クラン募集一覧ページをスタイリング

---
+++ components/Clan/ClanItem.vue
@@ -0,0 +1,112 @@
+<template>
+  <v-card
+    :id="`clan-${clan.id}`"
+    rounded-xl
+    hover
+    raised
+  >
+    <v-card
+      flat
+      nuxt
+      :to="`clans/${clan.id}`"
+    >
+      <v-container>
+        <v-card-title>
+          {{ clan.name }}
+        </v-card-title>
+
+        <v-divider />
+
+        <v-card-subtitle>
+          クラン活動の目標
+        </v-card-subtitle>
+
+        <v-card-text>
+          {{ clan.concept }}
+        </v-card-text>
+      </v-container>
+    </v-card>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ClanItem',
+  props: {
+    clan: {
+      type: Object,
+      default: () => {},
+      required: true,
+      id: {
+        type: Number,
+        required: true
+      },
+      name: {
+        type: String,
+        required: true
+      },
+      concept: {
+        type: String,
+        required: true
+      },
+      interview: {
+        type: String,
+        required: true
+      },
+      prohibited_matters: {
+        type: String,
+        required: true
+      },
+      enrollment: {
+        type: String,
+        required: true
+      },
+      activity_time: {
+        type: String,
+        required: true
+      },
+      enrollment_age: {
+        type: String,
+        required: true
+      },
+      snipe: {
+        type: String,
+        required: true
+      },
+      contact_means: {
+        type: String,
+        required: true
+      },
+      platform: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: String,
+        required: true
+      },
+      required_login: {
+        type: String,
+        required: true
+      },
+      required_ranked: {
+        type: String,
+        required: true
+      },
+      required_vc: {
+        type: Boolean,
+        required: true
+      },
+      personality: {
+        type: String,
+        required: true
+      },
+      host: {
+        type: Object,
+        default: () => {},
+        required: true
+      }
+    }
+  }
+}
+</script>
--- pages/clans/index.vue
+++ pages/clans/index.vue
@@ -1,11 +1,88 @@
 <template>
   <v-container>
-    ClansIndex
+    <v-row>
+      <v-col
+        v-for="(clan, i) in displayClans"
+        :key="i"
+        cols="12"
+        sm="6"
+        md="6"
+        lg="4"
+        xl="4"
+      >
+        <ClanItem :clan="clan" />
+      </v-col>
+
+      <v-tooltip
+        left
+        color="primary"
+      >
+        <template #activator="{ on, attrs }">
+          <v-fab-transition>
+            <v-btn
+              color="primary"
+              dark
+              fixed
+              bottom
+              right
+              fab
+              style="z-index: 10;"
+              nuxt
+              to="/clans/create"
+              x-large
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-icon>mdi-plus</v-icon>
+            </v-btn>
+          </v-fab-transition>
+        </template>
+        <span>{{ $t('pages.clans-create') }}</span>
+      </v-tooltip>
+    </v-row>
+
+    <v-row dense>
+      <v-col>
+        <v-pagination
+          v-model="page"
+          :length="pageLength"
+          @input="setPageNumber"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
-  name: 'ClansIndexPage'
+  name: 'ClansIndexPage',
+  data () {
+    return {
+      page: 1,
+      pageSize: 12,
+      pageNumber: 0
+    }
+  },
+  computed: {
+    clans () {
+      return this.$store.getters['clans/clans']
+    },
+    pageLength () {
+      return Math.ceil(this.clans.length / this.pageSize)
+    },
+    displayClans () {
+      this.returnTop()
+
+      return this.pageNumber !== 0 ? this.clans.slice(this.pageSize * (this.pageNumber - 1), this.pageSize * this.pageNumber) : this.clans.slice(0, this.pageSize)
+    }
+  },
+  methods: {
+    setPageNumber (number) {
+      this.pageNumber = number
+    },
+    returnTop () {
+      window.scroll({ top: 0, behavior: 'smooth' })
+    }
+  }
 }
 </script>
