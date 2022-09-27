4b943ee36f79a173749108f86b7f7f15807842eb
nicozo <niczo.soccer161@gmail.com>
2022-09-27 23:33:16 +0900

    Add: ClanDetails.vueを作成

---
+++ components/Clan/ClanDetails.vue
@@ -0,0 +1,92 @@
+<template>
+  <v-card id="clan-details">
+    <v-container>
+      <v-card-title class="font-weight-bold">
+        {{ clan.name }}
+      </v-card-title>
+
+      <!-- コンセプト -->
+      <v-card
+        id="clan-concept"
+        flat
+      >
+        <v-card-title>
+          コンセプト
+        </v-card-title>
+
+        <v-divider />
+
+        <v-card-text>
+          {{ clan.concept }}
+        </v-card-text>
+      </v-card>
+
+      <v-row dense>
+        <!-- 募集要項 -->
+        <v-col
+          cols="12"
+          sm="6"
+          md="6"
+          lg="6"
+        >
+          <clan-requirements :requirements="clan.requirements" />
+        </v-col>
+
+        <!-- クラン情報 -->
+        <v-col
+          cols="12"
+          sm="6"
+          md="6"
+          lg="6"
+        >
+          <clan-information :information="clan.information" />
+        </v-col>
+      </v-row>
+
+      <!-- 面接情報 -->
+      <v-card
+        id="clan-interview"
+        flat
+      >
+        <v-card-title>
+          面接について
+        </v-card-title>
+
+        <v-divider />
+
+        <v-card-text>
+          {{ clan.interview }}
+        </v-card-text>
+      </v-card>
+
+      <!-- 禁止事項 -->
+      <v-card
+        id="clan-prohibited_matters"
+        flat
+      >
+        <v-card-title>
+          禁止事項
+        </v-card-title>
+
+        <v-divider />
+
+        <v-card-text>
+          {{ clan.prohibited_matters }}
+        </v-card-text>
+      </v-card>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ClanDetails',
+  props: {
+    clan: {
+      type: Object,
+      require: true,
+      default: () => {}
+    }
+  }
+}
+</script>
--- pages/clans/_id/index.vue
+++ pages/clans/_id/index.vue
@@ -8,83 +8,7 @@
 
     <v-row>
       <v-col cols="12">
-        <v-card id="clan-details">
-          <v-container>
-            <v-card-title>
-              {{ clan.name }}クラン
-            </v-card-title>
-
-            <!-- コンセプト -->
-            <v-card
-              id="clan-concept"
-              flat
-            >
-              <v-card-title>
-                コンセプト
-              </v-card-title>
-
-              <v-divider />
-
-              <v-card-text>
-                {{ clan.concept }}
-              </v-card-text>
-            </v-card>
-
-            <v-row dense>
-              <!-- 募集要項 -->
-              <v-col
-                cols="12"
-                sm="6"
-                md="6"
-                lg="6"
-              >
-                <clan-requirements :requirements="clan.requirements" />
-              </v-col>
-
-              <!-- クラン情報 -->
-              <v-col
-                cols="12"
-                sm="6"
-                md="6"
-                lg="6"
-              >
-                <clan-information :information="clan.information" />
-              </v-col>
-            </v-row>
-
-            <!-- 面接情報 -->
-            <v-card
-              id="clan-interview"
-              flat
-            >
-              <v-card-title>
-                面接について
-              </v-card-title>
-
-              <v-divider />
-
-              <v-card-text>
-                {{ clan.interview }}
-              </v-card-text>
-            </v-card>
-
-            <!-- 禁止事項 -->
-            <v-card
-              id="clan-prohibited_matters"
-              flat
-            >
-              <v-card-title>
-                禁止事項
-              </v-card-title>
-
-              <v-divider />
-
-              <v-card-text>
-                {{ clan.prohibited_matters }}
-              </v-card-text>
-            </v-card>
-          </v-container>
-        </v-card>
+        <clan-details :clan="clan" />
       </v-col>
     </v-row>
   </v-container>
@@ -94,9 +18,9 @@
 export default {
   name: 'ClanIdIndex',
   middleware: ['clan'],
-  computed: {
-    clan () {
-      return this.$store.getters['clans/clan']
+  data () {
+    return {
+      clan: this.$store.getters['clans/clan']
     }
   }
 }
