4cac2f58f842a6f11e56e4f9c5bbdc47a5cb7573
nicozo <niczo.soccer161@gmail.com>
2022-09-28 01:31:42 +0900

    Add: クラン編集ページの雛形とミドルウェアを作成

---
+++ pages/clans/_id/update.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+    clan: {{ clan }}
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'ClanUpdatePage',
+  middleware: ['clan-edit'],
+  data () {
+    return {
+      clan: this.$store.getters['clans/clan']
+    }
+  }
+}
+</script>
