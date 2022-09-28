c09decba209909e0f18b68d1978a75e3a2dffcec
nicozo <niczo.soccer161@gmail.com>
2022-09-29 00:14:44 +0900

    Add: Myクランページを作成

--- pages/clans/my_clan.vue
+++ pages/clans/my_clan.vue
@@ -1,10 +1,49 @@
 <template>
   <v-container>
-    <v-row>
-      <v-col cols="12">
-        <clan-details :clan="clan" />
+    <v-row dense>
+      <v-col>
+        <clan-back-previous-button />
       </v-col>
     </v-row>
+
+    <template v-if="my_clan">
+      <v-row>
+        <v-col cols="12">
+          <clan-details :clan="my_clan" />
+        </v-col>
+      </v-row>
+    </template>
+
+    <template v-else>
+      <v-row
+        justify="center"
+        align="center"
+      >
+        <v-col>
+          <v-card
+            flat
+            class="text-center"
+          >
+            <v-container>
+              <v-card-text>
+                作成した募集中のクランはありません。
+              </v-card-text>
+
+              <v-card-actions>
+                <v-btn
+                  block
+                  color="success"
+                  nuxt
+                  :to="{ name: 'clans-create' }"
+                >
+                  {{ $t('btn.clan_create') }}
+                </v-btn>
+              </v-card-actions>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </template>
   </v-container>
 </template>
 
@@ -14,7 +53,7 @@ export default {
   middleware: ['my-clan'],
   data () {
     return {
-      clan: this.$store.getters['clans/clan']
+      my_clan: this.$store.getters['clans/myClan']
     }
   }
 }
