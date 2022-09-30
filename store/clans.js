c02be54042658be659ad05fbcebaa82539acb7d7
nicozo <niczo.soccer161@gmail.com>
2022-10-01 00:07:53 +0900

    Memo: Todoコメントを追加

--- store/clans.js
+++ store/clans.js
@@ -2,12 +2,16 @@ export const state = () => ({
   clans: [],
   clan: {},
   myClan: {}
+  // Todo 検索機能をVuexで管理する考えだったが良策が思い浮かばないため一旦コメントアウト
+  // filteredClans: []
 })
 
 export const getters = {
   clans: state => state.clans,
   clan: state => state.clan,
   myClan: state => state.myClan
+  // Todo 検索機能をVuexで管理する考えだったが良策が思い浮かばないため一旦コメントアウト
+  // filteredClans: state => state.filteredClans
 }
 
 export const mutations = {
@@ -32,6 +36,27 @@ export const mutations = {
   deleteClan (state, deleteClan) {
     state.clans = state.clans.filter(clan => clan.id !== deleteClan.id)
   }
+  // Todo 検索機能をVuexで管理する考えだったが良策が思い浮かばないため一旦コメントアウト
+  // setFilteredClans (state, filterQuery) {
+  //   console.log('filter!!')
+  //   const clans = []
+
+  //   for (const i in state.clans) {
+  //     const clan = state.clans[i]
+
+  //     if (clan.platform.includes(filterQuery.platform) &&
+  //         clan.age.includes(filterQuery.age) &&
+  //         clan.required_login.includes(filterQuery.required_login) &&
+  //         clan.required_ranked.includes(filterQuery.required_ranked)
+  //         // clan.required_vc.includes(filterQuery.required_vc)
+  //     ) {
+  //       clans.push(clan)
+  //     }
+  //   }
+
+  //   console.log(clans)
+  //   state.clans = clans
+  // }
 }
 
 export const actions = {
@@ -53,4 +78,8 @@ export const actions = {
   deleteClan ({ commit }, res) {
     commit('deleteClan', res)
   }
+  // Todo 検索機能をVuexで管理する考えだったが良策が思い浮かばないため一旦コメントアウト
+  // getFilteredClans ({ commit }, filterQuery) {
+  //   commit('setFilteredClans', filterQuery)
+  // }
 }
