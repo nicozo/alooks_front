165d371334d69fb5fb3543f4145523928021c557
nicozo <niczo.soccer161@gmail.com>
2022-10-03 21:31:34 +0900

    Add: ApplicationApplicantProfile.vueを作成

---
+++ components/Application/ApplicationApplicantProfile.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-card>
+    <v-container>
+      <v-card-title>
+        {{ $t('pages.profile') }}
+      </v-card-title>
+
+      <v-divider />
+
+      <v-card-subtitle class="pb-1">
+        -{{ $t('user.self_introduction') }}-
+      </v-card-subtitle>
+      <v-card-text>
+        {{ applicant.self_introduction }}
+      </v-card-text>
+
+      <v-card-subtitle class="pb-1 pt-0">
+        -{{ $t('user.sex') }}-
+      </v-card-subtitle>
+      <v-card-text>
+        {{ $t(`gender.${applicant.sex}`) }}
+      </v-card-text>
+
+      <v-card-subtitle class="pb-1 pt-0">
+        -{{ $t('user.date_of_birth') }}-
+      </v-card-subtitle>
+      <v-card-text>
+        {{ getUserAge(applicant.date_of_birth) }}歳
+      </v-card-text>
+
+      <v-card-subtitle class="pb-1 pt-0">
+        -{{ $t('user.kd') }}-
+      </v-card-subtitle>
+      <v-card-text>
+        {{ applicant.kd }}
+      </v-card-text>
+
+      <v-card-subtitle class="pb-1 pt-0">
+        -{{ $t('user.highest_damage') }}-
+      </v-card-subtitle>
+      <v-card-text>
+        {{ applicant.highest_damage }}
+      </v-card-text>
+
+      <v-card-subtitle class="pb-1 pt-0">
+        -{{ $t('user.favorite_weapons') }}-
+      </v-card-subtitle>
+      <v-card-text>
+        {{ applicant.favorite_weapons }}
+      </v-card-text>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ApplicationApplicantProfile',
+  props: {
+    applicant: {
+      type: Object,
+      default: () => {},
+      required: true
+    }
+  },
+  methods: {
+    getUserAge (birthday) {
+      // console.log(birthday)
+      if (!birthday) { return }
+      const today = new Date()
+      const ymd = birthday.split('-')
+      const thisYearsBirthday = new Date(today.getFullYear(), ymd[1] - 1, ymd[2])
+      const age = today.getFullYear() - ymd[0]
+
+      return today < thisYearsBirthday ? age - 1 : age
+    }
+  }
+}
+</script>
--- components/Application/ApplicationItem.vue
+++ components/Application/ApplicationItem.vue
@@ -52,57 +52,7 @@
           justify="center"
         >
           <v-col cols="12">
-            <v-card>
-              <v-container>
-                <v-card-title>
-                  {{ $t('pages.profile') }}
-                </v-card-title>
-
-                <v-divider />
-
-                <v-card-subtitle class="pb-1">
-                  -{{ $t('user.self_introduction') }}-
-                </v-card-subtitle>
-                <v-card-text>
-                  {{ application.applicant.self_introduction }}
-                </v-card-text>
-
-                <v-card-subtitle class="pb-1 pt-0">
-                  -{{ $t('user.sex') }}-
-                </v-card-subtitle>
-                <v-card-text>
-                  {{ $t(`gender.${application.applicant.sex}`) }}
-                </v-card-text>
-
-                <v-card-subtitle class="pb-1 pt-0">
-                  -{{ $t('user.date_of_birth') }}-
-                </v-card-subtitle>
-                <v-card-text>
-                  {{ getUserAge(application.applicant.date_of_birth) }}歳
-                </v-card-text>
-
-                <v-card-subtitle class="pb-1 pt-0">
-                  -{{ $t('user.kd') }}-
-                </v-card-subtitle>
-                <v-card-text>
-                  {{ application.applicant.kd }}
-                </v-card-text>
-
-                <v-card-subtitle class="pb-1 pt-0">
-                  -{{ $t('user.highest_damage') }}-
-                </v-card-subtitle>
-                <v-card-text>
-                  {{ application.applicant.highest_damage }}
-                </v-card-text>
-
-                <v-card-subtitle class="pb-1 pt-0">
-                  -{{ $t('user.favorite_weapons') }}-
-                </v-card-subtitle>
-                <v-card-text>
-                  {{ application.applicant.favorite_weapons }}
-                </v-card-text>
-              </v-container>
-            </v-card>
+            <application-applicant-profile :applicant="application.applicant" />
           </v-col>
 
           <v-col
@@ -301,16 +251,6 @@ export default {
     }
   },
   methods: {
-    getUserAge (birthday) {
-      // console.log(birthday)
-      if (!birthday) { return }
-      const today = new Date()
-      const ymd = birthday.split('-')
-      const thisYearsBirthday = new Date(today.getFullYear(), ymd[1] - 1, ymd[2])
-      const age = today.getFullYear() - ymd[0]
-
-      return today < thisYearsBirthday ? age - 1 : age
-    },
     onExpansionPanelClick (application) {
       if (event.currentTarget.classList.contains('v-expansion-panel-header--active')) {
         console.log('Panel is closing/now closed.')
