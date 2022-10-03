60fadf401254e06a7ff9c00c3b411b460a89c2bd
nicozo <niczo.soccer161@gmail.com>
2022-10-04 00:18:08 +0900

    Refactor: ESLintエラーを修正

--- components/Room/RoomEditAndDeleteButton.vue
+++ components/Room/RoomEditAndDeleteButton.vue
@@ -18,48 +18,48 @@
         persistent
         max-width="600px"
       >
-      <template #activator="{ on, attrs }">
-        <v-btn
-          color="error"
-          block
-          v-bind="attrs"
-          v-on="on"
-          :disabled="invalid"
-        >
-          {{ $t('btn.room_destroy') }}
-        </v-btn>
-      </template>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            color="error"
+            block
+            v-bind="attrs"
+            :disabled="invalid"
+            v-on="on"
+          >
+            {{ $t('btn.room_destroy') }}
+          </v-btn>
+        </template>
 
-      <v-card>
-        <v-container>
-          <v-card-title>
-            <span class="text-h5">
-              削除しますか？
-            </span>
-          </v-card-title>
+        <v-card>
+          <v-container>
+            <v-card-title>
+              <span class="text-h5">
+                削除しますか？
+              </span>
+            </v-card-title>
 
-          <v-card-actions>
-            <v-spacer />
+            <v-card-actions>
+              <v-spacer />
 
-            <v-btn
-              color="red darken-1"
-              text
-              @click="closeDialog"
-            >
-              {{ $t('btn.cancel') }}
-            </v-btn>
+              <v-btn
+                color="red darken-1"
+                text
+                @click="closeDialog"
+              >
+                {{ $t('btn.cancel') }}
+              </v-btn>
 
-            <v-btn
-              color="blue darken-1"
-              text
-              :loading="btnLoading"
-              @click="handleDelete"
-            >
-              {{ $t('btn.delete') }}
-            </v-btn>
-          </v-card-actions>
-        </v-container>
-      </v-card>
+              <v-btn
+                color="blue darken-1"
+                text
+                :loading="btnLoading"
+                @click="handleDelete"
+              >
+                {{ $t('btn.delete') }}
+              </v-btn>
+            </v-card-actions>
+          </v-container>
+        </v-card>
       </v-dialog>
     </v-col>
   </v-row>
