67651b6e8b9afb576049470ed04fb108f9b8d9a1
nicozo <niczo.soccer161@gmail.com>
2022-10-08 18:18:16 +0900

    Update: 参加リクエストCRUDを作成

--- pages/admin/applications.vue
+++ pages/admin/applications.vue
@@ -8,7 +8,7 @@
       <template #top>
         <v-toolbar flat>
           <v-toolbar-title>
-            {{ $t('admin.applications') }}
+            {{ $t(`pages.${routeName}`) }}
           </v-toolbar-title>
 
           <v-divider
@@ -17,20 +17,80 @@
             vertical
           />
 
+          <!-- 参加リクエスト編集モーダル -->
           <v-dialog
-            v-model="dialogDelete"
+            v-model="editDialog"
+            max-width="500px"
+          >
+            <v-card>
+              <v-card-title>
+                <span class="text-h5">
+                  {{ $t('admin.application_edit') }}
+                </span>
+              </v-card-title>
+
+              <validation-observer v-slot="{ invalid }">
+                <v-container>
+                  <v-row dense>
+                    <v-col cols="12">
+                      <validation-provider
+                        v-slot="{ errors }"
+                        name="リクエストメッセージ"
+                        rules="required|max:25"
+                      >
+                        <v-text-field
+                          v-model="application.body"
+                          label="リクエストメッセージ"
+                          counter="25"
+                          :error-messages="errors"
+                          required
+                          placeholder="よろしくお願いします！"
+                        />
+                      </validation-provider>
+                    </v-col>
+
+                    <v-card-actions>
+                      <v-spacer />
+                      <v-btn
+                        color="error"
+                        text
+                        @click="closeDialog"
+                      >
+                        {{ $t('btn.cancel') }}
+                      </v-btn>
+
+                      <v-btn
+                        color="blue darken-1"
+                        text
+                        :disabled="invalid"
+                        :loading="btnLoading"
+                        @click="update(application)"
+                      >
+                        {{ $t('btn.update') }}
+                      </v-btn>
+                    </v-card-actions>
+                  </v-row>
+                </v-container>
+              </validation-observer>
+            </v-card>
+          </v-dialog>
+
+          <!-- 参加リクエスト削除モーダル -->
+          <v-dialog
+            v-model="deleteDialog"
             max-width="500px"
           >
             <v-card>
               <v-card-title class="text-h5">
-                ユーザーを削除しますか？
+                参加リクエストID{{ application.id }}を削除しますか？
               </v-card-title>
+
               <v-card-actions>
                 <v-spacer />
                 <v-btn
-                  color="blue darken-1"
+                  color="error"
                   text
-                  @click="closeDelete"
+                  @click="closeDialog"
                 >
                   {{ $t('btn.cancel') }}
                 </v-btn>
@@ -38,7 +98,7 @@
                 <v-btn
                   color="blue darken-1"
                   text
-                  @click="deleteItemConfirm"
+                  @click="applicationDelete(application)"
                 >
                   {{ $t('btn.delete') }}
                 </v-btn>
@@ -53,24 +113,17 @@
         <v-icon
           small
           class="mr-2"
-          @click="editItem(item)"
+          @click="openEditDialog(item)"
         >
           mdi-pencil
         </v-icon>
+
         <v-icon
           small
-          @click="deleteItem(item)"
+          @click="openDeleteDialog(item)"
         >
           mdi-delete
         </v-icon>
-      </template>
-      <template #no-data>
-        <v-btn
-          color="primary"
-          @click="initialize"
-        >
-          Reset
-        </v-btn>
       </template>
     </v-data-table>
   </v-container>
@@ -87,17 +140,15 @@ export default {
 
     return { applications }
   },
-  data () {
+  data ({ $route }) {
     return {
+      routeName: $route.name,
       activePicker: null,
       menu: false,
-      dialog: false,
-      dialogDelete: false,
+      editDialog: false,
+      deleteDialog: false,
       editedIndex: -1,
-      editedItem: {
-        body: ''
-      },
-      defaultItem: {
+      application: {
         body: ''
       },
       number: 10,
@@ -111,39 +162,77 @@ export default {
       ]
     }
   },
-  watch: {
-    menu (val) {
-      val && setTimeout(() => (this.activePicker = 'YEAR'))
+  computed: {
+    btnLoading () {
+      return this.$store.getters.btnLoading
     }
   },
   methods: {
-    editItem (item) {
-      this.editedIndex = this.rooms.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+    async update (application) {
+      this.$store.dispatch('getBtnLoading', true)
+
+      await this.$axios.$patch(
+        `/api/v1/admin/applies/${application.id}`,
+        this.application
+      )
+        .then(res => this.updateSuccessful(res))
+        .catch(e => this.updateFailure(e))
     },
-    deleteItem (item) {
-      this.editedIndex = this.rooms.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialogDelete = true
+    updateSuccessful (res) {
+      console.log(res)
+      const msg = `参加リクエストID${res.id}を更新しました`
+      const color = 'success'
+      this.setToaster(msg, color)
+      this.$store.dispatch('getBtnLoading', false)
+      this.closeDialog()
+      this.$router.go({ path: this.$router.currentRoute.path, force: true })
     },
-    deleteItemConfirm () {
-      this.rooms.splice(this.editedIndex, 1)
-      this.closeDelete()
+    updateFailure ({ response }) {
+      this.$store.dispatch('getBtnLoading', false)
+      if (response && response.status === 400) {
+        const msg = '参加リクエストの更新に失敗しました'
+        return this.$store.dispatch('getToast', { msg })
+      }
     },
-    close () {
-      this.dialog = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
+    async applicationDelete (application) {
+      this.$store.dispatch('getBtnLoading', false)
+
+      await this.$axios.$delete(
+        `/api/v1/admin/applies/${application.id}`
+      )
+        .then(res => this.deleteSuccessful(res))
     },
-    closeDelete () {
-      this.dialogDelete = false
-      this.$nextTick(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
-        this.editedIndex = -1
-      })
+    deleteSuccessful (res) {
+      console.log(res)
+      const msg = `参加リクエストID${res.id}を削除しました`
+      const color = 'success'
+      this.setToaster(msg, color)
+      this.$store.dispatch('getBtnLoading', false)
+      this.closeDialog()
+      this.$router.go({ path: this.$router.currentRoute.path, force: true })
+    },
+    setApplication (application) {
+      this.editedIndex = this.applications.indexOf(application)
+      this.application = Object.assign({}, application)
+    },
+    openEditDialog (application) {
+      this.setApplication(application)
+      this.editDialog = true
+    },
+    openDeleteDialog (application) {
+      this.setApplication(application)
+      this.deleteDialog = true
+    },
+    closeDialog () {
+      this.newDialog = false
+      this.editDialog = false
+      this.deleteDialog = false
+    },
+    saveDate (date) {
+      this.$refs.menu.save(date)
+    },
+    setToaster (msg, color) {
+      return this.$store.dispatch('getToast', { msg, color })
     }
   }
 }
