572a58498e42ed43f902f1791dd0c96f7f71db1a
nicozo <niczo.soccer161@gmail.com>
2023-01-02 16:49:17 +0900

    Add: 戻るボタンを作成

---
+++ components/Player/PlayerBackPreviousButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-btn
+    id="back-previous-page-button"
+    plain
+    @click="$router.push('/players')"
+  >
+    <v-icon>
+      mdi-chevron-double-left
+    </v-icon>
+    {{ $t('btn.back_to_players_page') }}
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'PlayerBackPreviousButton'
+}
+</script>
